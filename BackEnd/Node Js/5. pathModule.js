0d3cb6f8eb674edd0f4accd299454cde21df0c9e
Aditya Kumar <adityaakumar.in@gmail.com>
2024-08-12 17:44:55 +0530

    Path Module - Node Js

---
+++ BackEnd/Node Js/5. pathModule.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let PATH = "/Users/aditya/Documents/Coding/Code Snippit - Sigma Web Dev."
+
+console.log(path.extname(PATH))
+console.log(path.dirname(PATH))
+console.log(path.basename(PATH))
+
+// console.log(path.join("/home/", "programs\\harry.txt")); - Not working for mac
