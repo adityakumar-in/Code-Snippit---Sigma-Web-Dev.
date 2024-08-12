366105598f8150ace94dc338d04874a62318f1c2
Aditya Kumar <adityaakumar.in@gmail.com>
2024-08-13 02:03:57 +0530

    Components & Props

---
+++ React Js/2. component & props.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./App.css"
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  return (
+    <>
+      {/* Sending Data as Props  */}
+      <Navbar logoText="Homey"/> 
+    </>
+  )
+}
+
+export default App;
