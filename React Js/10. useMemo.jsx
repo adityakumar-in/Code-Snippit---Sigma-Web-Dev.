1a45656480aa8a51f61599060598196e0bba1ad3
Aditya Kumar <adityaakumar.in@gmail.com>
2024-08-14 01:51:17 +0530

    useMemo - React Js

---
+++ React Js/10. useMemo.jsx
@@ -0,0 +1,40 @@
+// In React, when something changes, the component re-renders. And for the complex computation This can create a performance issue when the component is re-rendered again and again
+// We can use useMemo() hook when we want to specify that between re-renders, a value should remain the same. 
+// It is used to optimize the performance of the application. It is used for memoizing the value (So value compute only one times and saved in memory).
+
+import { useState, useMemo } from 'react'
+import './App.css'
+
+const nums = new Array(30_000_000).fill(0).map((_, i)=>{
+  return {
+    index: i,
+    isMagical: i===29_000_000
+  }
+})
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [numbers, setNumbers] = useState(nums)
+
+  // const magical = numbers.find(item=>item.isMagical===true) // Expensive Computation
+  const magical = useMemo(() => numbers.find(item=>item.isMagical===true), [numbers]) // [] is dependency array, if numbers changes then only it will recompute
+
+  return (
+    <>
+      <div>
+        <span>Magical number is {magical.index}</span>
+      </div>
+      <div className="card">
+        <button onClick={() => {
+          setCount((count) => count + 1);
+        
+        }}>
+          count is {count}
+        </button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
