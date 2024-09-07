6add8862cc8e3312befc7217da694334d01caadd
Aditya Kumar <adityaakumar.in@gmail.com>
2024-09-07 15:43:10 +0530

    Changed Routing Name

--- Next Js/2. App(File) based Routing/app/page.js
+++ Next Js/2. App(File) based Routing/app/page.js
@@ -1,5 +1,5 @@
 // This is the home page of Next App
-// if you want to go to /about then in Page(File) based Routing you have to make a file named 'about' and inside that you have to make page.js
+// if you want to go to /about then in App(File) based Routing you have to make a file named 'about' and inside that you have to make page.js
 // In Next Js it'll automatically detect the file named page.js and render it on the browser
 import React from 'react'
 
